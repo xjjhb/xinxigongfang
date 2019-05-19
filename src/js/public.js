a8a0418bd9c8252a3608ffb1903e64ea7a7227ce
wangqianggithub <978159754@qq.com>
2019-05-19 18:07:10 +0800

    xx

--- src/js/public.js
+++ src/js/public.js
@@ -8,8 +8,16 @@ function containerFitScreen() {
 		viewport_h = window.innerHeight,
 		el_w = 1920,
 		el_h = 1080;
-	var scaleX = viewport_w / el_w;
-	var scaleY = viewport_h / el_h;
-	el.style.transform = el.style.webkitTransform = "scale(" + scaleX + ","+scaleY+")";
-	//document.body.style.overflow = "hidden";
+
+
+	if((viewport_w/viewport_h).toFixed(2) == 1.33){     //4：3屏幕
+        var scaleX = viewport_w / el_w;
+        var scaleY = viewport_h / 1440;
+        el.style.height = '1440px';
+        el.style.transform = el.style.webkitTransform = "scale(" + scaleX +","+scaleY+")";
+    }else{  //16：9屏幕
+        var scaleX = viewport_w / el_w;
+        var scaleY = viewport_h / el_h;
+        el.style.transform = el.style.webkitTransform = "scale(" + scaleX +")";
+    }
 }
